599626294ff14e387a7dca95fdc97ad50cf31817
Paul Nikonowicz <pnikonowicz@pivotal.io>
2019-03-11 23:02:25 -0400

    Update extract_variable.go

--- extract_variable.go
+++ extract_variable.go
@@ -25,3 +25,29 @@ func After(platform, browser string, resize int, wasInitialized func() bool) {
 		// do something
 	}
 }
+
+func assert(expected bool, testFunction func() bool) {
+	actual := testFunction()
+	
+	if expected != actual {
+		panic(fmt.Sprintf("%q != %q", expected, actual))
+	}
+}
+
+func main() {
+	isMacOs := "MAC"
+	isIeBrowser := "IE"
+	wasResized := 1 // number > 0
+	isInitialized := func() bool {return true}
+	
+	notMacOs := "not mac"
+	notIeBrowser := "not ie"
+	notResized := -1 // number <= 0
+	notInitialized := func() bool {return false}
+	
+	assert(true, func() bool { Before(isMacOS, isIeBrowser, wasResized, isInitialized) })
+	assert(false, func() bool { Before(notMacOS, isIeBrowser, wasResized, isInitialized) })
+	assert(false, func() bool { Before(isMacOS, notIeBrowser, wasResized, isInitialized) })
+	assert(false, func() bool { Before(isMacOS, isIeBrowser, notResized, isInitialized) })
+	assert(false, func() bool { Before(isMacOS, isIeBrowser, wasResized, notInitialized) })
+}

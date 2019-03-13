c7787463617ccb2b69d03bfa132ec14c5369fc71
Paul Nikonowicz <pnikonowicz@pivotal.io>
2019-03-13 17:54:56 -0400

    WIP: fixing build and tests
    tests are still failing
    
    Signed-off-by: Paul Nikonowicz <pnikonowicz@pivotal.io>

--- extract_variable.go
+++ extract_variable.go
@@ -1,19 +1,21 @@
-package nyc_tdd_lesson
+package main
 
 import (
+	"fmt"
 	. "strings"
 )
 
 //For Page 124 Introduce Explaining Variable
-func Before(platform, browser string, resize int, wasInitialized func() bool) {
+func Before(platform, browser string, resize int, wasInitialized func() bool) bool {
 	if Compare(ToUpper(platform), "MAC") > -1 &&
 		Compare(ToUpper(browser), "IE") > -1 &&
 		wasInitialized() && resize > 0 {
-		// do something
+		return true
 	}
+	return false
 }
 
-func After(platform, browser string, resize int, wasInitialized func() bool) {
+func After(platform, browser string, resize int, wasInitialized func() bool) bool {
 	isMacOs := Compare(ToUpper(platform), "MAC") > -1
 	isIEBrowser := Compare(ToUpper(browser), "IE") > -1
 	wasResized := resize > 0
@@ -22,32 +24,39 @@ func After(platform, browser string, resize int, wasInitialized func() bool) {
 		isIEBrowser &&
 		wasInitialized() &&
 		wasResized {
-		// do something
+		return true
 	}
+	return false
 }
 
-func assert(expected bool, testFunction func() bool) {
-	actual := testFunction()
-	
+func assert(expected bool, actual bool) {
 	if expected != actual {
-		panic(fmt.Sprintf("%q != %q", expected, actual))
+		panic(fmt.Sprintf("Expected: [%v] but was: [%v]", expected, actual))
 	}
 }
 
-func main() {
+func test(testFunction func(string, string, int, func() bool) bool) {
 	isMacOs := "MAC"
 	isIeBrowser := "IE"
 	wasResized := 1 // number > 0
-	isInitialized := func() bool {return true}
-	
+	isInitialized := func() bool { return true }
+
 	notMacOs := "not mac"
 	notIeBrowser := "not ie"
 	notResized := -1 // number <= 0
-	notInitialized := func() bool {return false}
-	
-	assert(true, func() bool { Before(isMacOS, isIeBrowser, wasResized, isInitialized) })
-	assert(false, func() bool { Before(notMacOS, isIeBrowser, wasResized, isInitialized) })
-	assert(false, func() bool { Before(isMacOS, notIeBrowser, wasResized, isInitialized) })
-	assert(false, func() bool { Before(isMacOS, isIeBrowser, notResized, isInitialized) })
-	assert(false, func() bool { Before(isMacOS, isIeBrowser, wasResized, notInitialized) })
+	notInitialized := func() bool { return false }
+
+	assert(true, testFunction(isMacOs, isIeBrowser, wasResized, isInitialized))
+	assert(false, testFunction(notMacOs, isIeBrowser, wasResized, isInitialized))
+	assert(false, testFunction(isMacOs, notIeBrowser, wasResized, isInitialized))
+	assert(false, testFunction(isMacOs, isIeBrowser, notResized, isInitialized))
+	assert(false, testFunction(isMacOs, isIeBrowser, wasResized, notInitialized))
+}
+
+func main() {
+	fmt.Println("Running Suite: Before")
+	test(Before)
+
+	fmt.Println("Running Suite: After")
+	test(After)
 }

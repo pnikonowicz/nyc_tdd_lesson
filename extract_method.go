f58eb1c5939cdf213eb40bcdea7027cd3a052f71
Paul Nikonowicz <pnikonowicz@pivotal.io>
2019-03-18 17:45:10 -0400

    Update extract_method.go

--- extract_method.go
+++ extract_method.go
@@ -1,27 +1,53 @@
 //TODO: Book Example
-//TODO: Student Example
+//TODO: Student Excercise
 
 
 
 
 Before:
-void printOwing(double amount) {
-  printBanner()
+string printOwing(double amount, string name) {
+  banner = printBanner()
   
   //print details
-  print("name:" + _name)
-  print("amount:" + _amount)
+  banner += "name:" + _name
+  banner += "amount:" + _amount
+  
+  return banner
 }
 
 After:
 
-void printOwing(double amount) {
-  printBanner()
-  printDetails()
+string printOwing(double amount, string name) {
+  banner = printBanner()
+  details = printDetails()
   
+  return banner + details
 }
 
 void printDetails() {
   print("name:" + _name)
   print("amount:" + _amount)
+}
+
+
+Excercise:
+string printOwing() {
+  owingStr = ""
+  var print = (str) => {
+    owingStr += str
+  }
+  
+  //banner
+  print("****Banner")
+  
+  //calculate outstanding
+  while(element in elements) {
+    print("Element: " + element)
+  }
+  
+  //print details
+  print("name:" + name)
+  print("amount:" + amount)
+  
+  return owingStr;
 }
